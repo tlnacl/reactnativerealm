b101bb457e6beab03e33488ac3912b14a3d980db
Liang Tang <liang.tang@coretex.com>
2018-08-14 11:27:03 +1200

    in the middle of add location

--- app/location/LocationView.js
+++ app/location/LocationView.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 import LocationService from "../realm/LocationService";
 
 class LocationView extends Component {
@@ -14,29 +14,35 @@ class LocationView extends Component {
   }
 
   componentDidMount() {
+    this.location = LocationService.getLocation();
     LocationService.addListener(() => {
-      this.forceUpdate();
+      this.locationUpdate();
     });
     this.watchId = navigator.geolocation.watchPosition(
         (position) => {
           LocationService.upsertLocation({
             latitude: position.coords.latitude,
-            longitude: position.coords.longitude});
+            longitude: position.coords.longitude
+          });
         },
-        (error) => this.setState({ error: error.message }),
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+        (error) => this.setState({error: error.message}),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10},
     );
   }
 
   componentWillUnmount() {
     navigator.geolocation.clearWatch(this.watchId);
+    LocationService.removeListener();
   }
 
-
+  locationUpdate() {
+    console.log(`latitude: ${this.latitude}, longitude: ${this.longitude}`);
+    this.setState({latitude: LocationService.location.latitude, longitude: LocationService.location.longitude});
+  }
 
   render() {
     return (
-        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Text>Latitude: {this.state.latitude}</Text>
           <Text>Longitude: {this.state.longitude}</Text>
           {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
--- app/realm/LocationService.js
+++ app/realm/LocationService.js
@@ -1,9 +1,7 @@
 import RealmDb from "./RealmDb";
 
 const LocationService = {
-  getLocation: function() {
-    return RealmDb.get().objects('Location').filtered('name = "location"');
-  },
+  location : RealmDb.get().objects('Location').filtered('name = "location"'),
   upsertLocation: function(location) {
     RealmDb.get().write(() => {
       RealmDb.get().create('Location', {name:'location', ...location, updatedAt: new Date()}, true);
